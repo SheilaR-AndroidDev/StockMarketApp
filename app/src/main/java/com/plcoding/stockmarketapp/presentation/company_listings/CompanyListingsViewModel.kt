b64e3cc5bac52dc1a7e1dbd40b65cd1ae7c71a05
SheilaR-AndroidDev <ssromar@hotmail.com>
2022-09-19 17:56:46 -0500

    Company Listings View Model, State and Event Setup.

---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsViewModel.kt
@@ -0,0 +1,64 @@
+package com.plcoding.stockmarketapp.presentation.company_listings
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.plcoding.stockmarketapp.domain.repository.StocksRepositoryI
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.plcoding.stockmarketapp.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class CompanyListingsViewModel @Inject constructor(
+    private val repositoryI: StocksRepositoryI
+) : ViewModel() {
+
+    var state by mutableStateOf(CompanyListingsState())
+
+    private var searchJob: Job? = null
+
+    fun onEvent(event: CompanyListingsEvent){
+        when(event){
+            is CompanyListingsEvent.Refresh ->{
+                getCompanyListings(fetchFromRemote = true)
+            }
+            is CompanyListingsEvent.OnSearchQueryChange ->{
+                state = state.copy(searchQuery = event.query)
+                searchJob?.cancel()
+                searchJob = viewModelScope.launch {
+                    delay(500L)
+                    getCompanyListings()
+                }
+            }
+        }
+    }
+
+    private fun getCompanyListings(
+        query: String = state.searchQuery.lowercase(),
+        fetchFromRemote: Boolean = false
+    ){
+        viewModelScope.launch {
+            repositoryI.getCompanyListings(fetchFromRemote, query)
+                .collect{ result ->
+                    when(result){
+                        is Resource.Success -> {
+                            result.data?.let { listings ->
+                                state = state.copy(
+                                    companies = listings
+                                )
+                            }
+                        }
+                        is Resource.Error -> Unit
+                        is Resource.Loading ->{
+                            state = state.copy(isLoading = result.isLoading)
+                        }
+                    }
+                }
+        }
+    }
+}
